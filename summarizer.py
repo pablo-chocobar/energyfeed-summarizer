ebd0bd4971156b0e9cd32f447e8ae7e22af3c98e
pablo-chocobar <95429240+pablo-chocobar@users.noreply.github.com>
2023-01-27 09:21:30 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,8 +1,21 @@
 from transformers import pipeline
-summarizer = pipeline("summarization")
+summarizer = pipeline("summarization", model = "pablo-chocobar/summarizer")
 import sys 
 def summarize(arg1):
     x = arg1
+    
+    def clean(Article_text):
+        lines_list = []
+
+        for line in Article_text.split("\n"):
+
+            stripped_line = line.strip()
+            
+            if len(stripped_line) >= 100:
+                lines_list.append(stripped_line)
+        return "   ".join(lines_list)
+    
+    x = clean(x)
     article_text = """ """
     for i in x:
         article_text += i
@@ -10,9 +23,6 @@ def summarize(arg1):
     import re
     article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
     article_text = re.sub(r'\s+', ' ', article_text)
-
-    formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
-    formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
 
     article_text1  = article_text[:1000]
     summary = summarizer(article_text1, max_length=200, min_length=100, do_sample=False)
