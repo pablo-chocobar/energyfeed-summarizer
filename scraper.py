261f23a0d1df3676ce047dc22291f27ed436d539
pablo-chocobar <95429240+pablo-chocobar@users.noreply.github.com>
2023-01-27 09:20:43 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,10 +1,11 @@
 import sys
-def scrape(arg1):
+def scrape(url):
+    
+    min_length = 400
         
     from bs4 import BeautifulSoup as bs
     from selenium import webdriver
 
-    url = arg1
     chrome_driver = r"C:\Users\iampr\Downloads\chromedriver.exe"
 
     option = webdriver.ChromeOptions()
@@ -13,10 +14,40 @@ def scrape(arg1):
 
     driver.get(url) 
     soup = bs(driver.page_source, 'html.parser') 
+    [tag.extract() for tag in soup.find_all(
+    ["script", "img", "ol", "ul", "time", "h1", "h2", "h3", "iframe", "style", "form", "footer", "figcaption"])]
 
-    p = [z.get_text() for z in soup.find_all("p")]
+    noisy_names = ["image", "img", "video", "subheadline", "editor", "fondea", "resumen", "tags", "sidebar", "comment",
+                   "entry-title", "breaking_content", "pie", "tract", "caption", "tweet", "expert", "previous", "next",
+                   "compartir", "rightbar", "mas", "copyright", "instagram-media", "cookie", "paywall", "mainlist", "sitelist"]
 
-    return p
+    article_body = ""
+    for tag in soup.find_all("div"):
+
+        try:
+            tag_id = tag["id"].lower()
+
+            for item in noisy_names:
+                if item in tag_id:
+                    tag.extract()
+        except:
+            pass
+    
+    for article_tag in soup.find_all("article"):
+
+        if len(article_tag.text) >= len(article_body):
+            article_body = article_tag.text
+    
+    if len(article_body) <= min_length:
+
+        for tag in soup.find_all(["div", "section"]):
+            try:
+                if len(tag.text) >= len(article_body):
+                    article_body = tag.text
+            except:
+                pass
+
+    return article_body
 
 if __name__ == "__main__":
     scrape(sys.argv[1])
